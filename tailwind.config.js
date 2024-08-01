209f81a6933a7e93b3991523de1b400c4449c858
Autmug <magutujr@gmail.com>
2024-08-01 15:33:05 +0300

    added tailwindcss and postcss for the styling of the project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		'./src/**/*.{vue,js,ts,jsx,tsx}', './public/index.html'
+	],
+ 	darkMode: false, // or 'media' or 'class'
+	theme: {
+    	extend: {},
+  	},
+  	plugins: [],
+}
