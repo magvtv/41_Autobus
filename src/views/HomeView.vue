b374ac6ba722b30d414ebb9da5ccc92a05dcbd31
magvtv <magutujr@gmail.com>
2024-09-17 05:52:33 +0300

    add a second hero section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,30 @@
 <template>
 	<div>
 		<AppHeader />
-		<AppHero />
+        <!-- <AppHero /> -->
+		<AppHeros />
 		<TopBusRoutes />
+        <AppSponsors />
 		<AppFooter />
 	</div>
 </template>
 
 <script>
 import AppHeader from '../components/AppHeader.vue'
-import AppHero from '../components/AppHero.vue'
+// import AppHero from '../components/AppHero.vue'
+import AppHeros from '@/components/AppHeros.vue';
 import TopBusRoutes from '../components/TopBusRoutes.vue';
 import AppFooter from '@/components/AppFooter.vue';
+import AppSponsors from '@/components/AppSponsors.vue';
 
 
 export default {
 	components: {
-		AppHeader,
-		AppHero,
-		TopBusRoutes,
+        AppHeader,
+        // AppHero,
+        AppHeros,
+        TopBusRoutes,
+        AppSponsors,
 		AppFooter
 	}
 }
